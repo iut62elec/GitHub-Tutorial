772831e1a4606b248dd54d1b8bac5d199c7d357c
Man Peng <man.peng@nationalgrid.com>
2015-09-15 15:55:37 -0400

    my change

---
+++ most_basic_commands.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env 
+
+# first set up git info
+git config --global user.name "John Doe"
+git config --global user.email johndoe@example.com
+# check git config info
+git config --list
+
+# create a git repo
+git init
+
+# make change to codes 
+
+# clone a git 
