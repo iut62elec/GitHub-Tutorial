69de529eff627b278cf28aed650c8f8fe1929451
Man Peng <man.peng@nationalgrid.com>
2015-09-15 15:58:54 -0400

    my change

--- most_basic_commands.sh
+++ most_basic_commands.sh
@@ -9,6 +9,10 @@ git config --list
 # create a git repo
 git init
 
-# make change to codes 
+# make change to codes and commit LOCALLY
+git add dummy1.py *.py dummy_folder/*.txt
+git add --all #adding all files
+git commit -m 'my change to some codes'
 
-# clone a git 
+# clone a git on GitHub
+git clone https://github.com/ng-manpeng/markup.git
