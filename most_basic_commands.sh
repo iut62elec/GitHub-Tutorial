4d7167977af70cec804021468f545fe7997b9c62
Man Peng <man.peng@nationalgrid.com>
2015-09-16 00:48:53 -0400

    added some new commands

--- most_basic_commands.sh
+++ most_basic_commands.sh
@@ -15,7 +15,14 @@ git init
 git add dummy1.py *.py dummy_folder/*.txt
 git add --all #adding all files
 git commit -m 'my change to some codes'
+# check the current status, such as uncommitted changes, failures, etc.
+git status
 
+# Revert changes after the files are even staged (after git add steps)
+    # so that you may keep the files what they were before any of your
+    # current modificaitons
+git reset --hard  # Revert changes to modified files.
+git clean -fd  # Remove all untracked files and directories.
 
 # clone a git on GitHub
 git clone https://github.com/ng-manpeng/markup.git
@@ -33,16 +40,29 @@ git fetch --all ; git pull --all
 ##################################################
 
 # check out the remotes you currently have, usually the default
-# one is the "origin"
+    # one is the "origin"
 git remote -v
 # create your own branch
 git branch man-branch
 git branch -a #check the list of branches you have
 # switch to my own branch
 git checkout man-branch
-# After you changed your program codes and made commits
+# merge the branch named "man-branch" into the current branch "master"
+    # here, we assume your current branch is "master"
+git merge man-branch
+# after you changed your program codes and made commits
 git push -u origin man-branch # push your changes to github
-    # another way to push results is through github desktop software
+
+"""
+Another way to push results is through github desktop software
+
+If you are a collaborator who wants to make contribuiton to the 
+    main branch through GitHub, you may
+    1) fork the original branch into your own branch in your GitHub account
+    2) after you make changes to your forked branch, you may send a pull 
+        request for your branch (the base) to be merged into original
+        main branch (the HEAD)
+"""
 
 ################################################################
 ### Input from Yoon about the public key assess on EC2 Instance
